2bc140b3a8eeca5b117d82c04d842ebc80e14603
Franco Catrin <fcatrin@gmail.com>
2016-05-27 20:51:43 +0000

    Add activity for leanback default
    git-svn-id: https://svn.xtvapps.com/svn/unica/trunk/retrobox/android/Droid800Activity@4298 fb8f5c45-ab54-496a-8d9e-4c10816ea98d

---
+++ src/xtvapps/retrobox/atari800/AddOnActivity.java
@@ -0,0 +1,12 @@
+package xtvapps.retrobox.atari800;
+
+import retrobox.utils.AddOnInfoActivity;
+
+public class AddOnActivity extends AddOnInfoActivity {
+
+	@Override
+	protected String getInfo() {
+		return "The Atari 8 bit add-on for RetroBoxTV has been installed successfully";
+	}
+
+}
